cbad51bc80e875a0213693fc7004f2a4d0318881
Ariel Robi <91098890+aerobinson384@users.noreply.github.com>
2022-08-04 12:41:59 -0700

    Create quiz_where_nonnumeric.sql

---
+++ quiz_where_nonnumeric.sql
@@ -0,0 +1,6 @@
+/*Practice Question Using WHERE with Non-Numeric Data
+Filter the accounts table to include the company name, website, and the primary point of contact (primary_poc) just for the Exxon Mobil company in the accounts table.*/
+
+SELECT name, website, primary_poc
+FROM accounts
+WHERE name = 'Exxon Mobil';
