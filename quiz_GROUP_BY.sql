798c3190f8a81ab9b29ead5f0fc0ffc69eb6a9ee
Ariel Robi <91098890+aerobinson384@users.noreply.github.com>
2022-08-23 14:37:37 -0700

    Create quiz_GROUP_BY.sql

---
+++ quiz_GROUP_BY.sql
@@ -0,0 +1,16 @@
+/*Questions: GROUP BY
+One part that can be difficult to recognize is when it might be easiest to use an aggregate or one of the other SQL functionalities. Try some of the below to see if you can differentiate to find the easiest solution.*/
+
+/*Which account (by name) placed the earliest order? Your solution should have the account name and the date of the order.*/
+
+/*Find the total sales in usd for each account. You should include two columns - the total sales for each company's orders in usd and the company name.*/
+
+/*Via what channel did the most recent (latest) web_event occur, which account was associated with this web_event? Your query should return only three values - the date, channel, and account name.*/
+
+/*Find the total number of times each type of channel from the web_events was used. Your final table should have two columns - the channel and the number of times the channel was used.*/
+
+/*Who was the primary contact associated with the earliest web_event?*/
+
+/*What was the smallest order placed by each account in terms of total usd. Provide only two columns - the account name and the total usd. Order from smallest dollar amounts to largest.*/
+
+/*Find the number of sales reps in each region. Your final table should have two columns - the region and the number of sales_reps. Order from fewest reps to most reps.*/
