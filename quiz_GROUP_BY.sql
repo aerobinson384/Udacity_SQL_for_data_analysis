6842310f2d6fc86d80a7e89fff88da35fc9a2f7d
Ariel Robi <91098890+aerobinson384@users.noreply.github.com>
2022-08-26 09:58:14 -0700

    Update quiz_GROUP_BY.sql

--- quiz_GROUP_BY.sql
+++ quiz_GROUP_BY.sql
@@ -20,18 +20,16 @@ GROUP BY a.name;
 
 /*Via what channel did the most recent (latest) web_event occur, which account was associated with this web_event? Your query should return only three values - the date, channel, and account name.*/
 
-SELECT w.channel, o.occurred_at date, a.name account
-FROM orders o
-JOIN accounts a
-ON a.id = o.account_id
+SELECT w.channel, w.occurred_at date, a.name account
+FROM accounts a
 JOIN web_events w 
 ON w.account_id = a.id
-ORDER BY o.occurred_at DESC
+ORDER BY w.occurred_at DESC
 LIMIT 1;
 
 /*Find the total number of times each type of channel from the web_events was used. Your final table should have two columns - the channel and the number of times the channel was used.*/
 
-SELECT w.channel, COUNT(W.channel)
+SELECT w.channel, COUNT(w.channel)
 FROM web_events w
 GROUP BY w.channel;
 
@@ -41,11 +39,23 @@ SELECT a.primary_poc
 FROM accounts a 
 JOIN web_events w
 ON w.account_id = a.id
-JOIN orders o
-ON o.account_id = a.id
-ORDER BY o.occurred_at DESC
+ORDER BY w.occurred_at DESC
 LIMIT 1;
 
 /*What was the smallest order placed by each account in terms of total usd. Provide only two columns - the account name and the total usd. Order from smallest dollar amounts to largest.*/
 
+SELECT a.name, MIN(o.total_amt_usd) AS min
+FROM accounts a
+JOIN orders o 
+ON o.account_id = a.id
+GROUP BY a.name
+ORDER BY min;
+
 /*Find the number of sales reps in each region. Your final table should have two columns - the region and the number of sales_reps. Order from fewest reps to most reps.*/
+
+SELECT COUNT(s.name), r.name
+FROM sales_reps s
+JOIN region r
+ON r.id = s.region_id
+GROUP BY r.name
+ORDER BY COUNT(s.name);
