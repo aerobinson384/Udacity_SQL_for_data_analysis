861c8ff92aef425656d4d54f5030b07fc522563a
Ariel Robi <91098890+aerobinson384@users.noreply.github.com>
2022-09-10 22:18:59 -0700

    Update quiz_DISTINCT.sql

--- quiz_DISTINCT.sql
+++ quiz_DISTINCT.sql
@@ -1,5 +1,24 @@
 /*Questions: DISTINCT
 Use DISTINCT to test if there are any accounts associated with more than one region.*/
+SELECT a.name account, r.name region
+FROM accounts a
+JOIN sales_reps s
+ON a.sales_rep_id = s.id
+JOIN region r
+ON s.region_id = r.id;
 
+SELECT DISTINCT id, name
+FROM accounts;
+
+SELECT a.id as "account id", r.id as "region id", 
+a.name as "account name", r.name as "region name"
+FROM accounts a
+JOIN sales_reps s
+ON s.id = a.sales_rep_id
+JOIN region r
+ON r.id = s.region_id;
+
+SELECT DISTINCT id, name
+FROM accounts;
 
 /*Have any sales reps worked on more than one account?*/
