702be405300c844fba143ffd4b6a502b22e003de
Ariel Robi <91098890+aerobinson384@users.noreply.github.com>
2022-09-11 17:05:40 -0700

    Create quiz_DATE_fxns.sql

---
+++ quiz_DATE_fxns.sql
@@ -0,0 +1,14 @@
+/*Questions: Working With DATEs
+Find the sales in terms of total dollars for all orders in each year, ordered from greatest to least. Do you notice any trends in the yearly sales totals?*/
+
+
+/*Which month did Parch & Posey have the greatest sales in terms of total dollars? Are all months evenly represented by the dataset?*/
+
+
+/*Which year did Parch & Posey have the greatest sales in terms of total number of orders? Are all years evenly represented by the dataset?*/
+
+
+/*Which month did Parch & Posey have the greatest sales in terms of total number of orders? Are all months evenly represented by the dataset?*/
+
+
+/*In which month of which year did Walmart spend the most on gloss paper in terms of dollars?*/
