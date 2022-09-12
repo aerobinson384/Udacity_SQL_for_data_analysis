dcd6437c5a2ad4dea122550c55632bad7482c4a3
Ariel Robi <91098890+aerobinson384@users.noreply.github.com>
2022-09-11 17:40:15 -0700

    Update quiz_DATE_fxns.sql

--- quiz_DATE_fxns.sql
+++ quiz_DATE_fxns.sql
@@ -1,6 +1,10 @@
 /*Questions: Working With DATEs
 Find the sales in terms of total dollars for all orders in each year, ordered from greatest to least. Do you notice any trends in the yearly sales totals?*/
 
+SELECT SUM(o.total_amt_usd) total_usd, DATE_PART('year', o.occurred_at) year_
+FROM orders o
+GROUP BY 2
+ORDER BY 1 DESC;
 
 /*Which month did Parch & Posey have the greatest sales in terms of total dollars? Are all months evenly represented by the dataset?*/
 
